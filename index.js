b31bdb276b7425e1347c2a20da4c4a83d3f5a129
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:21:45 -0300

    Aula

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+app.use(express.json());
 
-const port = 3000;
+let personList = []
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const port = 3001;
+
+app.get('/visualizar', (req, res) => {
+    res.send(personList);
 });
+
+app.get('/params/:id', (req, res) => {
+    const { id } = req.params;
+    
+    res.send(id);
+});
+
+
+app.post('/cadastrar', (req,res) => {
+    const { name, age } = req.body;
+    personList.push({name, age})
+    res.send(`Usuario recebido ${name}`)
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
